085b0905b64ae4b1817c749b39dc4b43877ca46c
CoolCream <mahasankarshant@gmail.com>
2026-09-06 13:47:08 +0530

    created a simple program to ask user their name and show it. learnt fget with this

--- main.c
+++ main.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 #include <windows.h>
+#include <string.h>
 
-void printBanner() {
+void printBanner()
+{
     printf(
         "╔█████ ╔██████ ╔█████ ╔██ ╔██    ╔█████\n"
         "║██    ║██  ██ ║██    ║██ ║██    ║██\n"
         "║██    ║██████ ║█████ ║██████┌███║██\n"
-        "║██    ║██     ╚═══██ ║██═╗██└──┘║██\n"  
+        "║██    ║██     ╚═══██ ║██═╗██└──┘║██\n"
         "║█████ ║██     ╔█████ ║██ ║██    ║█████\n"
         "╚════╝ ╚═╝     ╚════╝ ╚═╝ ╚═╝    ╚════╝\n"
-        "            C Command Line             \n"
-    );
+        "            C Command Line             \n");
 }
 
-int main(void) {
+int main(void)
+{
     SetConsoleOutputCP(65001);
     SetConsoleCP(65001);
 
-    char name[] = "CPSH";
-    char learning[] = "C programming";
-    int version = 1;
-    char language = 'C';
+    char name[50];
 
-    printf("String: %s\n", name);
-    printf("Version: %d\n", version);
-    printf("language: %c\n", language);
-    printf("Learning: %s\n", learning);
-    
+    printf("Whats ur name!!!? ");
+    fgets(name, sizeof(name), stdin);
+
+    name[strcspn(name, "\n")] = '\0';
+
+    printf("Hello, %s\n", name);
+
     return 0;
 }
